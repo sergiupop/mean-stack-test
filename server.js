d55800c12633384a1f0d610f8c9c365713cb6cab
Sergiu Pop <sergiupop94@gmail.com>
2020-09-01 09:45:35 +0300

    Test API endpoints

--- server.js
+++ server.js
@@ -31,11 +31,30 @@ function handleError(res, reason, message, code) {
 }
 
 app.get("/api/contacts", function(req, res) {
-
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get contacts.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
 });
 
 app.post("/api/contacts", function(req, res) {
+    var newContact = req.body;
+    newContact.createDate = new Date();
 
+    if (!req.body.name) {
+        handleError(res, "Invalid user input", "Must provide a name.", 400);
+    } else {
+        db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+            if (err) {
+                handleError(res, err.message, "Failed to create new contact.");
+            } else {
+                res.status(201).json(doc.ops[0]);
+            }
+        });
+    }
 });
 
 app.get("/api/contacts/:id", function(req, res) {
